f384817f55360363b545ebf2c32baabc1592ef0f
Allen Timothe <allen.timothe@gmail.com>
2022-05-09 10:58:08 -0400

    Goals controller and routes setup

---
+++ dependencies.js
@@ -0,0 +1,7 @@
+//npm i -D nodemon
+//npm i dotenv mongoose express colors
+
+// Edits to package.json
+//   "scripts": {
+//  "start": "node backend/server.js",
+//  "server": "nodemon backend/server.js"
